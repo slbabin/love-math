bef6be4d56c1b495a9bf59fddc5ea503fc6218ca
slbabin <>
2023-05-10 22:42:52 +0000

    Add favicon and the enter press function

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,16 @@ document.addEventListener("DOMContentLoaded",function() {
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
-            }
-        
+            }     
          })
     }
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if(event.key === "Enter") {
+            checkAnswer();
+        }
+     });
+
+
     runGame("addition");
 })
 
@@ -23,6 +29,9 @@ document.addEventListener("DOMContentLoaded",function() {
  * and after the user's answer has been processed. 
  */
  function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
@@ -33,7 +42,7 @@ document.addEventListener("DOMContentLoaded",function() {
     } else if(gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
     }  
-    else if(gameType === "subtract") {
+    else if(gameType === "subtruct") {
         displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
